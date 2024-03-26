a54d882e71f88981ccfa293647a659b3ee92d664
Tom SALON <tom.salon-CIC@ibm.com>
2024-03-26 14:26:54 +0100

    feat(init) : react + tailwind setup

---
+++ src/views/Homepage/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+    return (
+        <div className="">
+            This will be the homepage of a netflix clone !
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    colors: {
+      'red': '#E50914',
+      'dark-red': '#B81D24',
+      'black': '#221F1F',
+      'white': '#FFF1'
+    },
+  },
+  plugins: [],
+}
+
