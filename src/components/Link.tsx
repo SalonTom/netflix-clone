555ca0bd439da6664d9283408c0f0d475c1d0209
Tom SALON <tom.salon-CIC@ibm.com>
2024-03-27 11:39:03 +0100

    feat(navbar) : responsiveness

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,12 @@
-export default function Link({linkName} : { linkName : string}) {
+export default function Link({linkName, onClick} : { linkName : string, onClick? : () => void }) {
+    const handleClick = () => {
+        if(onClick) {
+            onClick();
+        }
+    }
+
     return (
-        <div className="cursor-pointer ease-in-out duration-200 hover:text-netflix-grey">
+        <div className="text-netflix-grey py-2 cursor-pointer ease-in-out duration-200 hover:text-white lg:text-white lg:hover:text-netflix-grey lg:py-0" onClick={handleClick}>
             { linkName }
         </div>
     )
